9ace7ad254809bfc97f1e46885767093a6974c34
Dali_Raki <Dali_Raki@DESKTOP-KJDD8OP>
2024-11-07 20:11:27 +0100

    Add project files.

---
+++ Agile2TP2/Models/BokingModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Agile2TP2.Models
+{
+    public class BokingModel
+    {
+        public string name { get; set; }
+        public string room { get; set; }
+        public int hour { get; set; }
+        public string phoneNumber { get; set; }
+        public DateTime date { get; set; }
+
+    }
+}
---
+++ Agile2TP2/Services/BokingService.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Agile2TP2.Services
+{
+    using System.Collections.Generic;
+    using Agile2TP2.Models;
+	public class BokingService
+    {
+		BokingModel BokingModel { get; set; }
+
+
+		public void addMeeting(List<BokingModel> lsitBooking, BokingModel userinput)
+		{
+			lsitBooking.Add(userinput);
+		}
+
+
+        public bool IsBookingConflict(List<BokingModel> listBooking, BokingModel newBooking)
+        {
+            return listBooking.Any(existingBooking =>
+                existingBooking.room == newBooking.room &&
+                existingBooking.date.Date == newBooking.date.Date &&
+                ((newBooking.date >= existingBooking.date && newBooking.date < existingBooking.date.AddHours(existingBooking.hour)) ||
+                 (existingBooking.date >= newBooking.date && existingBooking.date < newBooking.date.AddHours(newBooking.hour))));
+        }
+
+    }
+}
